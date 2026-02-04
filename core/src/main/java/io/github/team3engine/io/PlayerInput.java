d62f0329c176fd83bc3b4f5740c10ae601601e67
genevivi <rporax@gmail.com>
2026-02-04 20:09:28 +0800

    playerinput use onKey method to get input instead of  in update method

--- core/src/main/java/io/github/team3engine/io/PlayerInput.java
+++ core/src/main/java/io/github/team3engine/io/PlayerInput.java
@@ -12,6 +12,12 @@ public class PlayerInput extends InputListener {
 
     @Override
     public boolean onKey(int keycode, boolean pressed) {
+        leftHeld  = Gdx.input.isKeyPressed(Keys.A) || Gdx.input.isKeyPressed(Keys.LEFT);
+        rightHeld = Gdx.input.isKeyPressed(Keys.D) || Gdx.input.isKeyPressed(Keys.RIGHT);
+        upHeld    = Gdx.input.isKeyPressed(Keys.W) || Gdx.input.isKeyPressed(Keys.UP);
+        downHeld  = Gdx.input.isKeyPressed(Keys.S) || Gdx.input.isKeyPressed(Keys.DOWN);
+
+        if (leftHeld || rightHeld || upHeld || downHeld) return true;
         return false;
     }
 
@@ -22,10 +28,7 @@ public class PlayerInput extends InputListener {
 
     @Override
     public void update(float deltaTime) {
-        leftHeld  = Gdx.input.isKeyPressed(Keys.A) || Gdx.input.isKeyPressed(Keys.LEFT);
-        rightHeld = Gdx.input.isKeyPressed(Keys.D) || Gdx.input.isKeyPressed(Keys.RIGHT);
-        upHeld    = Gdx.input.isKeyPressed(Keys.W) || Gdx.input.isKeyPressed(Keys.UP);
-        downHeld  = Gdx.input.isKeyPressed(Keys.S) || Gdx.input.isKeyPressed(Keys.DOWN);
+        
     }
 
     public void reset() {
