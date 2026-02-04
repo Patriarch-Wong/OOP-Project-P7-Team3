75eee3648a2037e0aeadec5daee96721855f8e38
Lavadew <94545838+Lavadew@users.noreply.github.com>
2026-02-04 16:43:18 +0800

    fix

--- core/src/main/java/io/github/team3engine/io/PlayerInput.java
+++ core/src/main/java/io/github/team3engine/io/PlayerInput.java
@@ -1,76 +1,32 @@
 package io.github.team3engine.io;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 
-import io.github.team3engine.entity.Circle;
-
 public class PlayerInput extends InputListener {
-    private Circle player;
-    private boolean leftHeld;
-    private boolean rightHeld;
-    private boolean upHeld;
-    private boolean downHeld;
+    private boolean leftHeld, rightHeld, upHeld, downHeld;
 
-    public PlayerInput(Circle player) {
-        this.player = player;
-        setActive(true); // enable by default
-    }
-    
-    // called on key input
-    @Override
-    public boolean onKey(int keycode, boolean pressed) {
-        if (keycode == Keys.A) {
-            leftHeld = pressed;
-            return true;
-        }
-        if (keycode == Keys.D) {
-            rightHeld = pressed;
-            return true;
-        }
-        if (keycode == Keys.W) {
-            upHeld = pressed;
-            return true;
-        }
-        if (keycode == Keys.S) {
-            downHeld = pressed;
-            return true;
-        }
-        return false; //input not consumed
-    }
-
-    // called on mouse input
-    @Override
-    public boolean onClick(int x, int y, int button) {
-        // button is left click, right click, middle click etc
-        // x and y is the coordinates
-        return false;
+    // This must be empty to match "new PlayerInput()" in Main.java
+    public PlayerInput() {
+        setActive(true);
     }
 
     @Override
     public void update(float deltaTime) {
-        // if (leftHeld) {
-        //     bucket.setX(bucket.getX() - deltaTime * bucket.getSpeed());
-        // }
-
-        // if (rightHeld) {
-        //     bucket.setX(bucket.getX() + deltaTime * bucket.getSpeed());
-        // }
+        leftHeld  = Gdx.input.isKeyPressed(Keys.A) || Gdx.input.isKeyPressed(Keys.LEFT);
+        rightHeld = Gdx.input.isKeyPressed(Keys.D) || Gdx.input.isKeyPressed(Keys.RIGHT);
+        upHeld    = Gdx.input.isKeyPressed(Keys.W) || Gdx.input.isKeyPressed(Keys.UP);
+        downHeld  = Gdx.input.isKeyPressed(Keys.S) || Gdx.input.isKeyPressed(Keys.DOWN);
     }
 
-    public boolean isLeftHeld() {
-        return leftHeld;
+    public void reset() {
+        leftHeld = rightHeld = upHeld = downHeld = false;
     }
 
+    public boolean isLeftHeld() { return leftHeld; }
+    public boolean isRightHeld() { return rightHeld; }
+    public boolean isUpHeld() { return upHeld; }
+    public boolean isDownHeld() { return downHeld; }
     
-    public boolean isRightHeld() {
-        return rightHeld;
-    }
-
-    public boolean isUpHeld() {
-        return upHeld;
-    }
-
-    public boolean isDownHeld() {
-        return downHeld;
-    }
+    @Override public boolean onKey(int keycode, boolean pressed) { return false; }
 }
